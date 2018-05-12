83d50734a4296bfb9654b4cc4112a71d34f08879
serjiklobanov <lobanov.sa@phystech.edu>
2018-05-13 00:39:06 +0300

    issues solved

--- generate.py
+++ generate.py
@@ -1,51 +1,48 @@
 import random
 import pickle
+import argparse
 
 
-def print_help():
-    helps = open("generateHelp.txt", 'br').read()
-    helps = helps.decode('utf8')
-    for l in helps:
-        print(l, end='')
-    print()
+def get_population(dic):
+    return tuple(dic.keys())
 
 
-def parse_dictionary(dic):
-    new_dictionary = {}
-    for word1 in dic.keys():
-        new_dictionary.update({word1: []})
-        for word2 in dic[word1].keys():
-            new_dictionary[word1] += [word2] * dic[word1][word2]
-    return new_dictionary
+def get_weights(dic):
+    weights = []
+    for word in get_population(dic):
+        weights += [dic[word]]
+    return tuple(weights)
 
 
-Command = input()
-while Command != "":
-    Command = Command.split()
-    if "--help" in Command:
-        print_help()
+parser = argparse.ArgumentParser(description="Command parser")
+parser.add_argument("--seed", type=str, dest="Seed", help="Optional argument. The initial word. If not specified, "
+                                                          "select the word randomly from all words (not including the "
+                                                          "frequency).")
+parser.add_argument("--model", type=str, dest="Model", help="The path to the file from which the model will be loaded.")
+parser.add_argument("--length", type=int, dest="length", help="Length of the generated sequence.")
+parser.add_argument("--output", dest="output", help="Optional argument. The file to which the result will be recorded."
+                                                    " If there is no argument, output to stdout.")
+while True:
+    Command = parser.parse_args(input().split())
+    with open(Command.Model, "rb") as inputFile:
+        Dictionary = pickle.load(inputFile)
+    if Command.Seed is not None:
+        Seed = Command.Seed
     else:
-        model = Command[Command.index("--model") + 1]
-        with open(model, "rb") as inputFile:
-            Dictionary = pickle.load(inputFile)
-        NewDictionary = parse_dictionary(Dictionary)
-        if "--seed" in Command:
-            seed = Command[Command.index("--seed") + 1]
-        else:
-            seed = random.choice(list(NewDictionary.keys()))
-        length = int(Command[Command.index("--length") + 1])
-        print(seed, end=' ')
-        if "--output" in Command:
-            output = open(Command[Command.index("--output") + 1])
-            for i in range(length):
-                seed = random.choice(NewDictionary[seed])
-                output.write(seed + ' ')
-                if (i + 1) % 20 == 0:
-                    output.write('\n')
-        else:
-            for i in range(length):
-                seed = random.choice(NewDictionary[seed])
-                print(seed, end=' ')
-                if (i + 1) % 20 == 0:
-                    print()
-    Command = input()
+        Seed = random.choice(list(Dictionary.keys()))
+    if Command.output is not None:
+        output = open(Command.output, "w")
+        for i in range(Command.length):
+            output.write(Seed + ' ')
+            Seed = random.choices(get_population(Dictionary[Seed]), weights=get_weights(Dictionary[Seed]), k=1)[0]
+            output.write(Seed + ' ')
+            if (i + 1) % 20 == 0:
+                output.write('\n')
+        output.close()
+    else:
+        print(Seed, end=' ')
+        for i in range(Command.length):
+            Seed = random.choices(get_population(Dictionary[Seed]), weights=get_weights(Dictionary[Seed]), k=1)[0]
+            print(Seed, end=' ')
+            if (i + 1) % 20 == 0:
+                print()
--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 from os import listdir
 import pickle
 import re
+from collections import defaultdict
+import argparse
 
 
 def parse(raw):
@@ -8,50 +10,32 @@ def parse(raw):
     return text
 
 
-def print_help():
-    helps = open("generateHelp.txt", 'br').read()
-    helps = helps.decode('utf8')
-    for l in helps:
-        print(l, end='')
-    print()
-
-
 def get_dictionary(input_dir):
-    last_word = ""
-    dic = {}
+    last_word = "*START*"
+    dic = defaultdict(lambda: defaultdict(int))
     for file in listdir(input_dir):
-        input_file = open(input_dir + '\\' + file, 'r')
+        input_file = open(input_dir + "\\" + file, "r")
         for line in input_file:
             string = line
             if LC:
                 string = string.lower()
             parsed = parse(string)
             for word in parsed:
-                if last_word != "":
-                    if word in dic[last_word].keys():
-                        dic[last_word][word] += 1
-                    else:
-                        dic[last_word].update({word: 1})
-                if word not in dic.keys():
-                    dic.update({word: {}})
+                dic[last_word][word] += 1
                 last_word = word
+    dic = dict(dic)
     return dic
 
 
-Command = input()
-while Command != "":
-    Command = Command.split()
-    if "--help" in Command:
-        print_help()
-    else:
-        LC = False
-        if "--lc" in Command:
-            LC = True
-        InputDir = Command[Command.index("--input-dir") + 1]
-        Model = Command[Command.index("--model") + 1]
-        Dictionary = get_dictionary(InputDir)
-        with open(Model, "wb") as outputFile:
-            pickle.dump(Dictionary, outputFile)
-        print('Ready')
-        outputFile.close()
-    Command = input()
+parser = argparse.ArgumentParser(description="Command parser")
+parser.add_argument("--input-dir", type=str, dest="InputDir", help="The path to the directory where "
+                                                                   "is the collection of documents")
+parser.add_argument("--model", type=str, dest="Model", help="The path to the file where the model will be saved.")
+parser.add_argument("--lc", action="store_true", dest="lc", help="Optional argument. Bring texts to lowercase.")
+Command = parser.parse_args(input().split())
+LC = Command.lc
+Dictionary = get_dictionary(Command.InputDir)
+with open(Command.Model, "wb") as outputFile:
+    pickle.dump(Dictionary, outputFile)
+print("Ready")
+outputFile.close()
